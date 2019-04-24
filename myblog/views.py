831abf6c726047e046175dccb49d2db0e2cad61f
disman1229 <346025232@qq.com>
2019-04-24 22:13:42 +0800

    block

--- myblog/views.py
+++ myblog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'myblog/base.html')
+    title:'文章列表'
+    context:{'title':title}
+    print (type(context))
+    return render(request, 'myblog/article_list.html',context)
