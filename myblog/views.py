01c8cf41c3ab5d17f01a531b44ba0190c6b3b9aa
disman1229 <346025232@qq.com>
2019-04-25 16:52:06 +0800

    构建数据库模型

--- myblog/views.py
+++ myblog/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    title:'文章列表'
-    context:{'title':title}
-    print (type(context))
-    return render(request, 'myblog/article_list.html',context)
+    return render(request, 'myblog/article_list.html')
